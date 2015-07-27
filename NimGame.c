02e9155426f65a50acafb41c6a37cc13197b3b32
ziv <zivbenster@gmail.com>
2015-07-27 12:32:35 +0300

    update

--- NimGame.c
+++ NimGame.c
@@ -3,18 +3,29 @@
 //
 
 #include <stdio.h>
-
-#define LINE_DELIMITER "---------------\n"
-#define PRINT_BALLZ(X)({for (int i =0; i < (X); i++){printf("o");} printf("\n");})
-#define SWITCH_PLAYER()({if (player==1){player = 2;} else{player = 1;}})
+//constants
 #define MAX_BALLZ_2_TAKE 3
+#define LINE_DELIMITER "---------------\n"
 
-int player = 1;
-int box1Balls = 0;
+/**
+ * macro that prints the balls in a certain box.
+ */
+#define PRINT_BALLZ(X)({for (int i =0; i < (X); i++){printf("o");} printf("\n");})
+/**
+ * macro that switches the player from 1 to 2 or 2 to 1.
+ */
+#define SWITCH_PLAYER()( (player==1) ? (player = 2) : (player = 1))
+
+// global vars.
+int player = 1;         // player defaults to player 1.
+int box1Balls = 0;      // boxes default to 0.
 int box2Balls = 0;
 
-
+/**
+ * helper method to print the board.
+ */
 void printBoard() {
+
     printf(LINE_DELIMITER);
     printf("Box 1: ");
     PRINT_BALLZ(box1Balls);
@@ -24,17 +35,25 @@ void printBoard() {
 
 }
 
+/**
+ * method that gets the move, validates box choice and processes the move.
+ */
 void getMove() {
-    int *boxChoice = 0;
-    printf("Player %d, i'ts your turn.\nPlayer %d, choose a box (1 or 2):", player, player);
-    scanf("%1d", boxChoice);
-    if (boxChoice != (int *) 1 & boxChoice != (int *) 2) {
+    int boxChoice = (int)0;
+
+    // get user input.
+    printf("Player %d, i'ts your turn.\nPlayer %d, choose a box (1 or 2):\n", player, player);
+    scanf("%1d", &boxChoice);
+
+    // check if box choice is OK, then reference the box pointer to the actual box.
+    if (boxChoice != 1 & boxChoice !=  2) {
         int *temp = 0;
         scanf("%1d", temp);
         getMove();
     } else {
+        // pointer to a box.
         int *currentBox = 0;
-        switch ((int)&boxChoice) {
+        switch (boxChoice) {
             case 1:
                 currentBox = &box1Balls;
                 break;
@@ -42,25 +61,33 @@ void getMove() {
                 currentBox = &box2Balls;
                 break;
         }
-        printf("Player %d, how many balls do you want to take from box %d?", player, (int) boxChoice);
-        int *numOfBallz = NULL;
-        scanf("%1d", numOfBallz);
-        if (numOfBallz < 0) {
-            printf("Number of balls to take must be positive");
-        } else if (numOfBallz > (int *) MAX_BALLZ_2_TAKE) {
-            printf("Cannot take more than 3 balls at a time.");
-        } else if (numOfBallz > (int) currentBox) {
-            printf("Cannot take more balls than what's in the box");
-        }
-        else {
-            *currentBox -= (int) &numOfBallz;
+        int numOfBallz;
+        int inValidMove = 1;
+
+        while (inValidMove){
+            printf("Player %d, how many balls do you want to take from box %d?\n", player, boxChoice);
+            scanf("%10d", &numOfBallz);
+
+            // check if pick is valid
+
+            if (numOfBallz < 0) {
+                printf("Number of balls to take must be positive.\n");
+            } else if (numOfBallz > MAX_BALLZ_2_TAKE) {
+                printf("Cannot take more than 3 balls at a time.\n");
+            } else if (numOfBallz > *currentBox) {
+                printf("Cannot take more balls than what's in the box.\n");
+            }
+            else {
+                *currentBox -= numOfBallz;
+                inValidMove = 0;
+            }
         }
     }
 
 }
 
 void startGame() {
-    while (box1Balls != 0 || box2Balls != 0) {
+    while (box1Balls != 0 && box2Balls != 0) {
         printBoard();
         getMove();
         SWITCH_PLAYER();
@@ -74,11 +101,11 @@ void getBalls(int *pBox, int boxNum) {
         printf("How many balls in box %d?\n", boxNum);
         scanf("%2d", pBox);
         getchar();
-        if (&pBox < 0) {
-            printf("Number of balls in box must be positive.");
+        if (*pBox < 0) {
+            printf("Number of balls in box must be positive.\n");
         }
 
-    } while (pBox < 0);
+    } while (*pBox < 0);
 
 }
 
@@ -89,7 +116,7 @@ int main() {
 
     startGame();
 
-    printf("Player %d wins the game. \n", player);
+    printf("Player %d wins the game.\n", player);
 
     return 0;
 }
--- StringChange.c
+++ StringChange.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
 
-int main2() {
+// constants for ASCII table
+#define LESS_THAN_5_START 48
+#define LESS_THAN_5_END 52
+#define GEQ_5_START 53
+#define GEQ_5_END 57
+#define LOWER_CASE_START 97
+#define UPPER_CASE_START 65
+#define UPPER_CASE_END 90
+#define LOWE_CASE_END 122
+#define MAX_CHARS_TO_READ_FROM_USER_INPUT_DUDE 50
+
+int main() {
     const int UPPER_TO_LOWER_DIFF = 32;
-    char input[50];
-    char output[50];
+    char input[MAX_CHARS_TO_READ_FROM_USER_INPUT_DUDE];
+    char output[MAX_CHARS_TO_READ_FROM_USER_INPUT_DUDE];
+
     for (unsigned int i = 0; i<sizeof(input); i++){
         input[i]=output[i]=0;
     }
-    scanf("%50[a-zA-Z0-9 ]", input);
+    scanf("%50[^\n]", input);
 
     for (unsigned int i = 0; i < sizeof(input); i++) {
         int temp = (int)input[i];
-        if (temp >=48 && temp <= 52){
+        if (temp >=LESS_THAN_5_START && temp <= LESS_THAN_5_END){
             output[i] = '0';
         }
-        else if(temp >= 52 && temp <= 57){
+        else if(temp >= GEQ_5_START && temp <= GEQ_5_END){
             output[i] = '8';
         }
-        else if (temp >= 65 && temp <= 90) {
+        else if (temp >= UPPER_CASE_START && UPPER_CASE_END <= 90) {
             output[i] = (char)(input[i] + UPPER_TO_LOWER_DIFF);
-        } else if (temp >= 97 && temp <= 122) {
+        } else if (temp >= LOWER_CASE_START && temp <= LOWE_CASE_END) {
             output[i] = (char)(input[i] - UPPER_TO_LOWER_DIFF);
         } else {
             output[i]=input[i];
